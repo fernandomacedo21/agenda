bf38bcd92785521f74b33d6f8613b588d41946d6
Fernando Macedo <fernando.macedo.santana@gmail.com>
2021-01-10 17:19:01 -0300

    Primeiro commit

---
+++ src/test/java/com/fernandomacedo/agenda/AgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandomacedo.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
